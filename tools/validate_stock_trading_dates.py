fe5b0efa8967241bfe09a17aa5407aad67c6bb65
boundles <wangyang9113@gmail.com>
2025-05-20 23:53:40 +0800

    modify

--- tools/validate_stock_trading_dates.py
+++ tools/validate_stock_trading_dates.py
@@ -22,7 +22,7 @@ def get_trading_calendar(data_dir, start_date, end_date):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="验证个股数据日期与交易日历对齐")
     parser.add_argument(
-        "-save_dir", required=True, type=str, help="Directory of the files"
+        "-data_dir", required=True, type=str, help="Directory of the files"
     )
     args = parser.parse_args()
 
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     miss_trading_days = 0
     total_trading_days = 0
     stock_files = glob.glob(
-        os.path.join(os.path.join(args.save_dir, "features"), "*.csv")
+        os.path.join(os.path.join(args.data_dir, "features"), "*.csv")
     )
     for stock_file in stock_files:
         # 加载个股数据
@@ -40,16 +40,13 @@ if __name__ == "__main__":
         # 生成理论交易日历
         start_date = df["Date"].min()
         end_date = df["Date"].max()
-        trading_days = get_trading_calendar(args.save_dir, start_date, end_date)
+        trading_days = get_trading_calendar(args.data_dir, start_date, end_date)
 
         # 找出差异
         missing_dates = trading_days - data_dates  # 数据缺失的交易日
-        miss_stock_cnt += len(missing_dates) > 0
+        miss_stock_cnt += (len(missing_dates) > 0)
         miss_trading_days += len(missing_dates)
         total_trading_days += len(trading_days)
-
-        extra_dates = data_dates - trading_days  # 数据中多余的非交易日
-        assert len(extra_dates) == 0
 
         # 输出结果
         print("------------------------------------\n")
@@ -60,7 +57,6 @@ if __name__ == "__main__":
         print("\n差异检测结果:")
         print(f"数据缺失的交易日数量: {len(missing_dates)}")
         print(f"数据缺失的交易日量: {sorted(missing_dates)}")
-        print(f"数据中多余的非交易日数量: {len(extra_dates)}")
         print("------------------------------------\n")
 
     print(f"数据中有缺失的股票占比: {miss_stock_cnt / len(stock_files)}")
